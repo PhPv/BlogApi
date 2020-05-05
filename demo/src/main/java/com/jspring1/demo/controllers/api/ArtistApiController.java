73dbd7a66fadf3680454e09eb2171383a6413c47
Slava <slavok15@yandex.ru>
2020-05-05 21:42:45 +0400

    refactor

---
+++ demo/src/main/java/com/jspring1/demo/controllers/api/ArtistApiController.java
@@ -0,0 +1,36 @@
+package com.jspring1.demo.controllers.api;
+
+
+import com.jspring1.demo.model.MusicGroup;
+import com.jspring1.demo.repo.MusiсGroupRepository;
+import net.minidev.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.Optional;
+
+@RestController
+public class ArtistApiController {
+
+    @Autowired
+    private MusiсGroupRepository repository;
+
+    @GetMapping(value = "api/group_get", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Optional<MusicGroup> groupGet(@RequestParam(value="id", defaultValue = "") String id) {
+        return repository.findById(id);
+    }
+
+    @RequestMapping(value = "api/group_add", method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public MusicGroup groupAdd(@RequestBody JSONObject body) {
+        MusicGroup result = new MusicGroup();
+        result.setName(body.get("name").toString());
+        result.setStyle(body.get("style").toString());
+        result.setCountry(body. get("country").toString());
+        repository.save(result);
+        return result;
+    }
+
+}
--- demo/src/main/java/com/jspring1/demo/controllers/api/BlogApiController.java
+++ demo/src/main/java/com/jspring1/demo/controllers/api/BlogApiController.java
@@ -1,10 +1,9 @@
-package com.jspring1.demo.controllers;
+package com.jspring1.demo.controllers.api;
 
 
-import com.jspring1.demo.model.MusicGroup;
+
 import com.jspring1.demo.model.Post;
 import com.jspring1.demo.repo.PostRepository;
-import com.jspring1.demo.repo.MusiсGroupRepository;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -14,21 +13,14 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-public class apiController {
+public class BlogApiController {
 
     @Autowired
     private PostRepository repository;
-    @Autowired
-    private MusiсGroupRepository repository2;
 
     @GetMapping(value = "api/post_get", produces = MediaType.APPLICATION_JSON_VALUE)
     public Optional<Post> postGet(@RequestParam(value="id", defaultValue = "") String id) {
         return repository.findById(id);
-    }
-
-    @GetMapping(value = "api/group_get", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Optional<MusicGroup> groupGet(@RequestParam(value="id", defaultValue = "") String id) {
-        return repository2.findById(id);
     }
 
     @DeleteMapping("api/post_del")
@@ -52,17 +44,6 @@ public class apiController {
         result.setAnons(body.get("anons").toString());
         result.setFull_text(body.get("full_text").toString());
         repository.save(result);
-        return result;
-    }
-
-    @RequestMapping(value = "api/group_add", method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public MusicGroup groupAdd(@RequestBody JSONObject body) {
-        MusicGroup result = new MusicGroup();
-        result.setName(body.get("name").toString());
-        result.setStyle(body.get("style").toString());
-        result.setCountry(body. get("country").toString());
-        repository2.save(result);
         return result;
     }
 
